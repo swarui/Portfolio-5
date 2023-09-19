73c5d49eb871ecccbf4ee0dd38a609ed193be327
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-19 11:04:28 +0300

    Completed personal portifolio

---
+++ script.js
@@ -0,0 +1,9 @@
+var elements = document.getElementById("#autotype");
+
+var typed = new Typed("#autotype", {
+  strings: ["Software developer", "Developer", "UI/UX designer"],
+  typeSpeed: 150,
+  backSpeed: 110,
+  loop: true
+  
+});
