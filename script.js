8ba13c73ff0efa3a76add55ceafdeb9466f2361d
swarui <stevewaruim@gmail.com>
2023-11-23 21:13:58 +0300

    Completed Project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var elements = document.getElementById("#autotype");
+let elements = document.getElementById("#autotype");
 
-var typed = new Typed("#autotype", {
+let typed = new Typed("#autotype", {
   strings: ["Software developer", "Developer", "UI/UX designer"],
   typeSpeed: 150,
   backSpeed: 110,
